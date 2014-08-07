5041bef533c564616c7507cee0ea268da6e1f750
Brandon Bey <brandonbey@gmail.com>
2014-08-06 20:51:53 -0700

    new models for chunk tiles, elements, and items

--- models/chunks.js
+++ models/chunks.js
@@ -2,16 +2,15 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 
+
 var chunkSchema = new Schema({
-
-  created:  { type: Date,   default: Date.now },
-  updated:  { type: Date,   default: Date.now },
-  x:        { type: Number, default: 0 },
-  y:        { type: Number, default: 0 },
-  tiles:    [ Schema.Types.Mixed ],
-  elements: [ Schema.Types.Mixed ],
-  items:    [ Schema.Types.Mixed ]
-
+  created:    { type: Date,   default: Date.now },
+  updated:    { type: Date,   default: Date.now },
+  x:          { type: Number, default: 0 },
+  y:          { type: Number, default: 0 },
+  _tiles:     [{ type: Schema.Types.ObjectId, ref: 'Tile' }],
+  _elements:  [{ type: Schema.Types.ObjectId, ref: 'Element' }],
+  _items:     [{ type: Schema.Types.ObjectId, ref: 'Item' }]
 });
 
 module.exports = mongoose.model('Chunk', chunkSchema);
